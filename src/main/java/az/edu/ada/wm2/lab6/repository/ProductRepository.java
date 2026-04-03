ca18e6efb131798cfee2b68e060f7ffdff6c4d37
Isa Salahov <97687195+isalahov12393@users.noreply.github.com>
2026-04-03 15:16:22 +0400

    Update ProductRepository.java

--- src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
+++ src/main/java/az/edu/ada/wm2/lab6/repository/ProductRepository.java
@@ -1,14 +1,13 @@
 package az.edu.ada.wm2.lab6.repository;
-
 import az.edu.ada.wm2.lab6.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.List;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.UUID;
 
-public interface ProductRepository {
-    Product save(Product product);
-    Optional<Product> findById(UUID id);
-    List<Product> findAll();
-    void deleteById(UUID id);
-    boolean existsById(UUID id);
+public interface ProductRepository extends JpaRepository<Product, UUID> {
+    List<Product> findByExpirationDateBefore(LocalDate date);
+    List<Product> findByPriceBetween(BigDecimal min, BigDecimal max);
 }
